2c423431d137768ff96d8b9a770b1dcbf325a297
Patrick <patasax22@yahoo.com>
2021-10-06 21:46:32 -0400

    hitting wall

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,6 +4,6 @@ test("creates new employee object", () => {
     const employee = new Employee('Jean');
 
     expect(employee.name).toBe('Jean');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe('email@email.com');
+    expect(employee.id).toBe(expect.any(String));
+    expect(employee.email).toBe(expect.any(String));
 });
--- __tests__/Intern.test.js
+++
@@ -1,8 +0,0 @@
-const intern = require('../lib/Intern');
-
-test("creates an intern object", () => {
-    const intern = new Intern('Jean');
-
-    expect(intern.name).toBe('Jean');
-    expect(intern.)
-})
